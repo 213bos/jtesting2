d5d0e95d237cecc8a1f141affac1c839137b052a
Andrew Boskovich <AndrewBoskovich@0587397691.wireless.umich.net>
2020-08-02 16:04:36 -0400

    fix / on resume link

--- js/bot.js
+++ js/bot.js
@@ -39,7 +39,7 @@ $(function() {
 	}
 	], { delay: 2000 })
 	.fbMessenger('typingIndicator', { delay: 2000 })
-	.fbMessenger('message', 'bot', 'Here are some important links! <a href="https://www.linkedin.com/in/andrew-boskovich/" target="_blank">LinkedIn</a> <a href="https://drive.google.com/file/d/19trWpChSqxIJdKBznw1yYCKn2-FhXXYM/view/" target="_blank">Resume</a>', {delay: 1500 })
+	.fbMessenger('message', 'bot', 'Here are some important links! <a href="https://www.linkedin.com/in/andrew-boskovich/" target="_blank">LinkedIn</a> <a href="https://drive.google.com/file/d/19trWpChSqxIJdKBznw1yYCKn2-FhXXYM/view" target="_blank">Resume</a>', {delay: 1500 })
 	.fbMessenger('typingIndicator', { delay: 500 })
 	.fbMessenger('message', 'bot', 'Scroll down for my recent work!', {delay: 1500 })
 	.fbMessenger('message', 'bot', 'Don\'t be shy, scroll down!', { timestamp: now, delay: 4000 })
